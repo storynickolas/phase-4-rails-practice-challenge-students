8a38904f1a559a094b490f0e1b947cd662d85d2c
Nick Story <storynickolasl@gmail.com>
2022-12-10 20:41:42 -0500

    Student and Instructor Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :students, only: [:index, :show, :create, :update]
+  resources :instructors, only: [:index, :show, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
